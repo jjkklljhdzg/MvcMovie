e23efe40829bd0d4f28aa8cd5f979cff2cf74847
jjkklljhdzg <sidorovasona00@gmail.com>
2025-05-12 16:38:38 +0400

    часть 5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using практика.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<практикаContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("практикаContext") ?? throw new InvalidOperationException("Connection string 'практикаContext' not found.")));
@@ -9,6 +10,12 @@ builder.Services.AddDbContext<практикаContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
